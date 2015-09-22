7b96b0244d7034c8ad442a7458b39f0e105e544b
Jill <pillanoid@gmail.com>
2015-09-21 18:20:55 -0700

    Add files

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using nancytest.Objects;
 
 namespace FriendLetterApp
 {
@@ -10,6 +11,13 @@ namespace FriendLetterApp
             Get["/"] = parameters => View["hello.html"];
             Get["/favorite_photos"] = parameters => View["favorite_photos.html"];
             Get["/form"] = parameters => View["form.html"];
+            Get["/greeting_card"] = parameters => {
+              var letterNames = new LetterNames {
+                Sender = this.Request.Query["Sender"],
+                Recipient = this.Request.Query["Recipient"]
+              };
+              return View["greeting-card.html", letterNames];
+            };
         }
     }
 }
